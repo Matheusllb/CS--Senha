acc9466ee9cc17618ceb0710460fde31ba50cbb3
Matheusllb <matheusluislopesbatista@gmail.com>
2023-04-04 20:01:33 -0300

    Tentativa de senha

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CsE15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCÍCIO 15
+            int senha = 2002; //Senha correta
+            int entrada = 0;
+            Console.WriteLine("Informe a senha correta\n\nSENHA:");
+            entrada = int.Parse(Console.ReadLine());
+            while (entrada != senha) 
+            {
+                Console.Clear();
+                Console.WriteLine("\nSenha Inválida!\n");
+                Console.WriteLine("Informe a senha correta\n\nSENHA:");
+                entrada = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Acesso Permitido!\nFim do Programa!\n");
+        }
+    }
+}
